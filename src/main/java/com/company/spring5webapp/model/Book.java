bdc0a6b72231e9157557bd3ec2c2d7ddab35247a
Igor Privalov <kingor333@mail.ru>
2018-01-05 23:56:23 +0300

    added Author and Book entities

---
+++ src/main/java/com/company/spring5webapp/model/Book.java
@@ -0,0 +1,77 @@
+package com.company.spring5webapp.model;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String title;
+    private String isdn;
+    private String publisher;
+
+    @ManyToMany
+    @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"),
+    inverseJoinColumns = @JoinColumn(name = "author_id"))
+    private Set<Author> authors = new HashSet<>();
+
+    public Book() {
+    }
+
+    public Book(String title, String isdn, String publisher) {
+        this.title = title;
+        this.isdn = isdn;
+        this.publisher = publisher;
+    }
+
+    public Book(String title, String isdn, String publisher, Set<Author> authors) {
+        this.title = title;
+        this.isdn = isdn;
+        this.publisher = publisher;
+        this.authors = authors;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getIsdn() {
+        return isdn;
+    }
+
+    public void setIsdn(String isdn) {
+        this.isdn = isdn;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public Set<Author> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(Set<Author> authors) {
+        this.authors = authors;
+    }
+}
